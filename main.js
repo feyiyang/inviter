bf4936254a311e1d1ea6818cc30116b3422294e7
yikeguo <isjianqiang@qq.com>
2024-06-19 23:48:02 +0800

    19

---
+++ main.js
@@ -0,0 +1,52 @@
+$(function () {
+  // const hei = $(window).height()
+  // $('.wrapper').css('width', Math.floor(hei * 0.6220338983050847))
+
+  const topimghei = $('.sec_1 .top_img img').height()
+  $('.sec_1 .top_img').css('height', topimghei * 2/3)
+
+  setTimeout(() => {
+    $('.sec_1').addClass('animed')
+  })
+
+  
+  $.scrollify({
+    section : ".section",
+    sectionName : "section-name",
+    interstitialSection : "",
+    easing: "linear",
+    scrollSpeed: 0,
+    offset : 0,
+    scrollbars: true,
+    standardScrollElements: "",
+    setHeights: true,
+    overflowScroll: true,
+    updateHash: true,
+    touchScroll:true,
+    before:function(index, sections) {
+      console.log('before scrollify')
+      if (index == 0) {
+        $('.sec_1').removeClass('animed')
+        return
+      }
+      $(sections[index]).find('.animate__animated').each(function () {
+        const cls = $(this).attr('class')
+        console.log(cls)
+        $(this).attr('class', cls.replace(/\s?animate__\w+/g, '') + ' animate__animated')
+      })
+    },
+    after:function(index, sections) {
+      console.log('after scrollify', index, sections)
+      if (index == 0) {
+        $('.sec_1').addClass('animed')
+        return
+      }
+      $(sections[index]).find('.animate__animated').each(function() {
+        const anim = $(this).attr('action')
+        $(this).addClass('animate__' + anim)
+      })
+    },
+    afterResize:function() {},
+    afterRender:function() {}
+  });
+})
